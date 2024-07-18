321ff5c8a365e434fad25f2351ef35c7547d908c
esawah <esaonee1@gmail.com>
2024-07-18 21:05:40 +0700

    up
    up up up up

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -5,7 +5,7 @@
     <h1>Welcome to the Admin Dashboard</h1>
     <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <button type="submit">Logout</button>
+        <button type="submit" class="bg-red-500 rounded-lg">Logout</button>
     </form>
 </body>
 </html>
